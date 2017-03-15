90b4b113520a4cef5f07f751c236d1d3084d42a7
ENGMENJARI <anjari1981@gmail.com>
2017-03-15 17:25:20 +0200

    feat admin

--- client/signin/signin.js
+++ client/signin/signin.js
@@ -5,9 +5,18 @@ app.controller('signinCtrl', function($scope, $window, $location, serv) {
   $scope.signin = function() {
     serv.signin($scope.user)
     .then(function(token) {
+      console.log(token.username)
+      if(token.username==='admin'){
+     $window.localStorage.setItem('MobileSmart', token.token);
+        $window.location.href='/#/AddMobiles'
+        
+      }else{
       $window.localStorage.setItem('MobileSmart', token);
       $location.path('/#/Apple');
-    })
+
+    }
+
+  })
     .catch(function(error) {
       console.error(error);
     });

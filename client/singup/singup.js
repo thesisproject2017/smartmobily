8c56041ecea80648cba3a09fb3e3dccfc1e22b2c
mohamed <toot2009@hotmail.com>
2017-03-17 22:57:42 +0200

    feat/add nodemiler and some chang in the style

--- client/singup/singup.js
+++ client/singup/singup.js
@@ -2,15 +2,21 @@
  var app = angular.module('MobileSmart.singup', []);
 
 app.controller('singupCtrl', function($scope, $window, $location, serv) {
+    $scope.userIsExest =false;
  $scope.user = {};
+
   $scope.signup = function() {
     serv.signup($scope.user)
     .then(function(token) {
+      $scope.userIsExest = false;
       $window.localStorage.setItem('MobileSmart', token);
       $location.path('/');
     })
     .catch(function(error) {
-      console.error(error);
+      if(error.status === 500){
+        $scope.userIsExest = true;
+        console.error(error);
+      }
     });
   };
 });

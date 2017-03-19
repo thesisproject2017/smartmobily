3ce953ae7617ed68fbd034442a6149f379674024
ENGMENJARI <anjari1981@gmail.com>
2017-03-19 11:03:06 +0200

    fix

--- client/signin/signin.js
+++ client/signin/signin.js
@@ -1,51 +1,27 @@
 var app = angular.module('MobileSmart.signin', []);
 
 app.controller('signinCtrl', function($scope, $window, $location, serv) {
-<<<<<<< HEAD
 $scope.user = {};
- $scope.signin = function() {
-   serv.signin($scope.user)
-   .then(function(token) {
-     if(token.username === 'admin'){
-       // alert('admin is')
-     $window.localStorage.setItem('MobileSmart', token.token);
-         $window.location.href="/#/AddMobiles"
+$scope.chakUser = false;
 
-     }else{
-     $window.localStorage.setItem('MobileSmart', token.token);
-     $location.path("/")
+$scope.signin = function() {
+ serv.signin($scope.user)
+    .then(function(token) {
+       $scope.chakUser = false;
+         if(token.username==='admin'){
+           $window.localStorage.setItem('MobileSmart', token.token);
+           $window.location.href='/#/AddMobiles'
 
+         }else{
+           $window.localStorage.setItem('MobileSmart', token.token);
+           $location.path('/');
 
-     }
-   })
-   .catch(function(error) {
-     console.error(error);
-   });
+         }
+
+       })
+     .catch(function(error) {
+       $scope.chakUser = true;
+       console.error(error);
+     });
  };
 });
-=======
- $scope.user = {};
- $scope.chakUser = false;
- 
- $scope.signin = function() {
-  serv.signin($scope.user)
-     .then(function(token) {
-        $scope.chakUser = false;
-          if(token.username==='admin'){
-            $window.localStorage.setItem('MobileSmart', token.token);
-            $window.location.href='/#/AddMobiles'
-
-          }else{
-            $window.localStorage.setItem('MobileSmart', token.token);
-            $location.path('/');
-
-          }
-
-        })
-      .catch(function(error) {
-        $scope.chakUser = true;
-        console.error(error);
-      });
-  };
-});
->>>>>>> 143a3132c753f09af36c0204b76b3040a9cd3e54
